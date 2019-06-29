4701d0883c8a39243159ada5680dd0e8e9e975d8
jeevcoding <jeevyaballal282@gmail.com>
2019-06-29 17:53:35 +0530

    solved displaying user info

--- app/src/main/java/com/jeevcode/notepad/NextActivity_1.java
+++ app/src/main/java/com/jeevcode/notepad/NextActivity_1.java
@@ -1,20 +1,28 @@
 package com.jeevcode.notepad;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 public class NextActivity_1 extends AppCompatActivity {
 
     Button logout_button;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
+    TextView uname;
+    TextView upass;
+    private String name;
+    private String email;
 
 
     @Override
@@ -27,18 +35,25 @@ public class NextActivity_1 extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next_1);
 
+
+        uname=(TextView)findViewById(R.id.user_name);
+        upass=(TextView)findViewById(R.id.user_pass);
         logout_button=(Button)findViewById(R.id.Logout);
         mAuth=FirebaseAuth.getInstance();
 
-        mAuthListener=new FirebaseAuth.AuthStateListener() {
+        mAuthListener=new FirebaseAuth.AuthStateListener()
+        {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth mAuth) {
+            public void onAuthStateChanged(@NonNull FirebaseAuth mAuth)
+            {
 
-                if(mAuth.getCurrentUser()==null)
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user == null)
                 {
 
 
@@ -49,7 +64,25 @@ public class NextActivity_1 extends AppCompatActivity {
 //                    startActivity(new Intent(NextActivity_1.this,MainActivity.class));
                     Toast.makeText(NextActivity_1.this,"Signing out!!",Toast.LENGTH_SHORT).show();
                 }
-                
+                else{
+
+                    for (UserInfo profile : user.getProviderData())
+                    {
+                        // Id of the provider (ex: google.com)
+                        //String providerId = profile.getProviderId();
+
+                        // UID specific to the provider
+                        //String uid = profile.getUid();
+
+                        // Name, email address, and profile photo Url
+                         name = profile.getDisplayName();
+                         email = profile.getEmail();
+                        uname.setText(name);
+                        upass.setText(email);
+                        //Uri photoUrl = profile.getPhotoUrl();
+                    }
+
+                }
 
             }
         };
@@ -57,9 +90,13 @@ public class NextActivity_1 extends AppCompatActivity {
 
 
 
-        logout_button.setOnClickListener(new View.OnClickListener() {
+        logout_button.setOnClickListener(new View.OnClickListener()
+
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+
+            {
                 //Toast.makeText(NextActivity_1.this,"Signing out () called!!",Toast.LENGTH_SHORT).show();
                 mAuth.signOut();
 
