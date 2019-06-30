16b7662dcf2c616ad5451679648c2fb1437b7a4d
jeevcoding <jeevyaballal282@gmail.com>
2019-06-30 14:45:48 +0530

    completed login,only forgot password left

--- app/src/main/java/com/jeevcode/notepad/loginActivity.java
+++ app/src/main/java/com/jeevcode/notepad/loginActivity.java
@@ -1,13 +1,18 @@
 package com.jeevcode.notepad;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import static android.os.Build.VERSION_CODES.O;
@@ -26,7 +31,9 @@ public class loginActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
@@ -40,6 +47,8 @@ public class loginActivity extends AppCompatActivity {
         firebaseAuth=FirebaseAuth.getInstance();
 
 
+
+
         enter_havent_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,9 +56,46 @@ public class loginActivity extends AppCompatActivity {
                 Intent intent = new Intent(loginActivity.this, MainActivity.class);
                 intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
-                finish();
+                finish();//finish i.e destroy  the ongoing activity after calling the next activity by intent
             }
         });
 
+        enter_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                validate(enter_username.getText().toString(),enter_pass.getText().toString());
+            }
+        });
+
+
+
     }
+
+
+    private void validate(String uname,String pass)
+
+    {
+
+
+        firebaseAuth.signInWithEmailAndPassword(uname,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(loginActivity.this,"Login Successful!",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(loginActivity.this, NextActivity_1.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+
+                }else{
+                    Toast.makeText(loginActivity.this,"Login Failed!",Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+    }
+
 }
